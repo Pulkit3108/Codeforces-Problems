4e828bc1ce4927748199d75bac36b27ccd281f38
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2020-05-05 18:02:57 +0530

    Create Round#628(Div. 2)A.cpp

---
+++ Round#628(Div. 2)A.cpp
@@ -0,0 +1,19 @@
+//GCD(a,b)+LCM(a,b)=x Find a,b
+#include<bits/stdc++.h>
+#include<numeric> 
+using namespace std;
+#define ll long long int
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    cin>>T;
+    while(T--){
+        ll x;
+        cin>>x;
+        cout<<1<<" "<<x-1<<endl;
+    }
+    
+    return 0;
+}
