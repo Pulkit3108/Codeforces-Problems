ce2805e4096f00a5fcc5bac19323f51215723595
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2020-06-20 12:21:34 +0530

    Create Round#633(Div. 2)A.cpp

---
+++ Round#633(Div. 2)A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    scanf("%d",&T);
+    while(T--){
+        ll n;
+        scanf("%lld",&n);
+        printf("%lld\n",n);
+
+
+    }
+    return 0;
+}
