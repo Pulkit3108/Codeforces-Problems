83f1b2dfb4d34fc8b8090d0b6706ba0c87e08ee0
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2020-07-13 11:33:39 +0530

    Create Round#655(Div. 2)A.cpp

---
+++ Round#655(Div. 2)A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){   
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    scanf("%d",&T);
+    while(T--){   
+        int n;
+        scanf("%d",&n);
+        int i;     
+        for(i=0;i<n;i++){
+            printf("%d ",1);
+        }
+        printf("\n");
+
+      
+    
+    }
+    return 0;
+}
