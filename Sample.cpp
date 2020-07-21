6c640e6ec669f1b0f0f024910e18df3184634568
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2020-07-21 13:50:04 +0530

    Update Sample.cpp

--- Sample.cpp
+++ Sample.cpp
@@ -40,6 +40,9 @@ using namespace std;
 #define sc2(a,b) scanf("%lld %lld",&a,&b)
 #define pf1(a) printf("%lld\n",a)
 #define pf2(a,b) printf("%lld %lld\n",a,b)
+#define vsort(v) sort(v.begin(), v.end())
+#define forn(i,n) for(ll i=0; i<n; i++)
+#define pb push_back
 int main()
 {
     ios_base::sync_with_stdio(false);
