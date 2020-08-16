704d631125caef3f4d8b7f7d545b10a96344af23
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2020-08-16 10:58:13 +0530

    Create Round#558(Div. 2)A.cpp

---
+++ Round#558(Div. 2)A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define sc1(a) scanf("%d",&a)
+#define sc2(a,b) scanf("%d %d",&a,&b)
+#define pf1(a) printf("%d\n",a)
+#define pf2(a,b) printf("%d %d\n",a,b)
+#define vsort(v) sort(v.begin(), v.end())
+#define forn(i,n) for(ll i=0; i<n; i++)
+#define pb push_back
+int main(){
+    int n,m;
+    sc2(n,m);
+    int c=m?min(m,n-m):1;
+    pf1(c);
+    return 0;
+}
