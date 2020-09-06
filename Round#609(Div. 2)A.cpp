18100ed11946e7f48f26c83f1f69c0fcc92105a8
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2020-09-06 11:29:47 +0530

    Create Round#609(Div. 2)A.cpp

---
+++ Round#609(Div. 2)A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t=1;
+    //cin>>t;
+    while(t--){
+        ll n;
+        scanf("%lld",&n);
+        printf("%lld %lld\n",(10*n),(9*n));
+    }
+    return 0; 
+} 
