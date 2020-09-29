21c93b8f445744048bfbe5f2df13ed6b57af90b3
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2020-09-29 10:06:13 +0530

    Update Round#674(Div. 3)A.py

--- Round#674(Div. 3)A.py
+++ Round#674(Div. 3)A.py
@@ -1,9 +1,10 @@
 t=int(input())
 for _ in range(t):
     n,x=map(int,input().split())
-    k=0
-    if(n<=2):
-        k=1
-    else:
-        k=(n-3)//x+2
+    k=1
+    if(n>2):
+        if((n-2)%x==0):
+            k+=(n-2)//x
+        else:
+            k+=(n-2)//x+1
     print(k)
