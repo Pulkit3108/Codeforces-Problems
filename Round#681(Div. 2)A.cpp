50fb2ea77700a551fb11a2b800eb248df0b2c306
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2020-11-05 13:10:34 +0530

    Create Round#681(Div. 2)A.cpp

---
+++ Round#681(Div. 2)A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        for(int i=0;i<n;i++){
+            cout<<(4*n-2*i)<<" ";
+        }
+        cout<<endl;
+    }
+}
+    
