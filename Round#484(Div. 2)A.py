1d474fa568ff0b2995e152817ffc92aee2fa1515
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2020-12-25 13:42:30 +0530

    Update Round#484(Div. 2)A.py

--- Round#484(Div. 2)A.py
+++ Round#484(Div. 2)A.py
@@ -1,6 +1,9 @@
 n=int(input())
 s=input()
-if('00' in s or '11' in s):
-    print('No')
-else:
+c=1
+if(('11' in s) or ('000' in s) or (n==1 and s=='0') or (s[0]=='0' and s[1]=='0') or (s[n-2]=='0' and s[n-1]=='0')):
+    c=0
+if(c==1):
     print('Yes')
+else:
+    print('No')
