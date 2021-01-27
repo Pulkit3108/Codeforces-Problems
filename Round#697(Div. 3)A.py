1cd90840d367755a3f96a4668213de9c7241427d
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2021-01-27 15:28:06 +0530

    Update Round#697(Div. 3)A.py

--- Round#697(Div. 3)A.py
+++ Round#697(Div. 3)A.py
@@ -1,8 +1,9 @@
 t=int(input())
 for _ in range(t): 
     n=int(input())
-    c=1
-    if(n%2!=0 or (n//2>1 and (n//2)%2!=0)):
+    while(n%2==0):
+        n=n//2
+    if(n>1 and n%2!=0):
         print('YES')
     else:
         print('NO')
