d7381d3c5c1c4dcd5e5bdbfb07dae12315412ae1
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2021-01-30 16:28:25 +0530

    Update Round#698(Div. 2)B.py

--- Round#698(Div. 2)B.py
+++ Round#698(Div. 2)B.py
@@ -2,18 +2,20 @@ t=int(input())
 for _ in range(t):
     q,d=map(int,input().split())
     a=list(map(int,input().split()))
-    m1=[ str(d*i) for i in range(1,11) ]
-    m2=[ i[-1] for i in m1 ]
-    for i in range(q):
-        s=-100
-        k=str(a[i])
-        for j in range(10):
-            if(m2[j]==k[-1]):
-                s=j
-                break
-        if(s<0 or int(m1[s])>a[i]):
-            print('NO')
-        else:
+    for i in a:
+        s=1
+        if(d*10>i):
+            x=d
+            s=i-x
+            while(s>0 and s%10!=0):
+                x+=d
+                s=i-x
+        if(s>=0):
             print('YES')
+        else:
+            print('NO')
+        
+        
+                
             
-       #
+        
