20400d41fb7e38858e358a6d3962a2c1bc5feba4
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2021-05-30 14:14:58 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -64,10 +64,18 @@ ll mul(ll a, ll b){
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
     ll T;
     sc1(T);
     while(T--){
-        
+        // code   
     }
+#ifndef ONLINE_JUDGE
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+#endif
     return 0;
 }
