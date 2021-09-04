8afb288840629e2e354861a79979251b048e0c94
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2021-09-04 14:25:05 +0530

    Update Round#439(Div. 2)B.py

--- Round#439(Div. 2)B.py
+++ Round#439(Div. 2)B.py
@@ -1,5 +1,9 @@
 a,b=map(int,input().split())
-k=1
-for i in range(a+1,b+1):
-   k=(k*i%10)&10
-print(k) 
+if(b-a>=10):
+    print(0)
+else:
+    k=1
+    for i in range(a+1,b+1):
+        k*=i%10
+        k%=10
+    print(k) 
