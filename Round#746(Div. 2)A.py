9c10facaf0638847c9cdd683a7138c241f00eccd
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2021-10-05 11:58:43 +0530

    Create Round#746(Div. 2)A.py

---
+++ Round#746(Div. 2)A.py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    n,H=map(int,input().split())
+    a=list(map(int,input().split()))
+    m1=max(a)
+    a.remove(m1)
+    m2=max(a)
+    if(H%(m1+m2)==0):
+        print(2*(H//(m1+m2)))
+    elif(H%(m1+m2)>m1):
+        print((2*(H//(m1+m2)))+2)
+    else:
+        print((2*(H//(m1+m2)))+1)
+            
