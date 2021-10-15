bf298500b905ef93b576d7534ef3653bcfd45a9d
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2021-10-15 12:33:47 +0530

    Update Round#748(Div. 3)A.py

--- Round#748(Div. 3)A.py
+++ Round#748(Div. 3)A.py
@@ -1,3 +1,3 @@
 for _ in range(int(input())):
     a,b,c=map(int,input().split())
-    print(max(0,1+max(b,c)-a),max(0,1+max(ac)-b),max(0,1+max(a,b)-c))
+    print(max(0,1+max(b,c)-a),max(0,1+max(a,c)-b),max(0,1+max(a,b)-c))
