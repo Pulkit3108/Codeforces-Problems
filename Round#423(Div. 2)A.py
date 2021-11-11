f4ebd77e37cf7db27c2a95c341f017224075154a
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2021-11-11 14:21:18 +0530

    Create Round#423(Div. 2)A.py

---
+++ Round#423(Div. 2)A.py
@@ -0,0 +1,23 @@
+n,a,b=map(int,input().split())
+t=list(map(int,input().split()))
+d=0
+s=0
+for i in range(n):
+    if(t[i]==1):
+        if(a==0):
+            if(b==0):
+                if(s==0):
+                    d+=1
+                else:
+                    s-=1
+            else:
+                b-=1
+                s+=1      
+        else:
+            a-=1
+    else:
+        if(b==0):
+            d+=2
+        else:
+            b-=1
+print(d)
