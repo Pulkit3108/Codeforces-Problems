47add6ba44734d0bc836988e6eabeace05dbc4e3
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2022-05-12 13:11:40 +0530

    Create Round#790(Div. 4)A.py

---
+++ Round#790(Div. 4)A.py
@@ -0,0 +1,11 @@
+for _ in range(int(input())):
+    s=input()
+    if(sum(map(int,s[:3]))==sum(map(int,s[3:]))):
+        print('YES')
+    else:
+        print('NO')
+    
+        
+        
+    
+    
